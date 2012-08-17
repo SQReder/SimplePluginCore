c023330e4a64c6314c88a9e6a289851c7321349c
endAurum <sqreder@gmail.com>
2012-08-17 14:58:24 +0400

    Задокументировал PluginInterface и начал HiveCore

--- src/Core/HiveCore.h
+++ src/Core/HiveCore.h
@@ -1,24 +1,60 @@
-﻿#pragma once
+﻿/** \file HiveCore.h
+  \brief Класс ядра проекта
+  \details Функции ложащиеся на ядро:
+  - Загрузка/выгрузка плагинов;
+  - Межплагиновое взаимодействие.
+  */
+#pragma once
 
 #include <QMap>
 #include <QStringList>
 
 class PluginInterface;
-typedef  QMap<PluginInterface*, QStringList> PluginMap;
 
+/** \brief Ядро системы
+    \details Реализовано паттерном "одиночка".
+    Инcтанцирование поизводится через  \ref HiveCore::Instance().
+*/
 class HiveCore
 {
+    /** \brief Список указателей на загруженные плагины с импортированными
+    методами */
+    typedef  QMap<PluginInterface*, QStringList> PluginMap;
 public:
+    /** \brief Пустой конструктор
+    \details По-хорошему нужно сделать его приватным, но Qt не дает мне этого
+    \todo пусть бросает исключение, чтоле? Ибо не должно вызываться нигде,
+        кроме метода \ref Instance() */
     HiveCore() {}
     virtual ~HiveCore() {}
 
+    /** \brief Создает статический экземпляр класса ядра и возвращает
+            указатель на него
+    \return Указатель на статический экземпляр ядра */
     static HiveCore* Instance() {
         static HiveCore* coreInstance = new HiveCore();
         return coreInstance;
     }
 
+    /** \brief Загрузка плагинов
+    \details Проматривает директорию <i>plugins</i> и пытается загрузить все
+        имеющиеся там файлы в качестве плагина.
+    \todo Сделать фильтр по типам файлов */
     void loadPlugins();
 
+    /** \brief Вызов импортированной функции по ее имени
+      \details Пытается найти переданное имя в списке импортированных функций
+            и вызвать, передав необходимые параметры
+      \param methodName Полное имя вызываемого метода.
+            Например "SomePlugin.SomeMethod"
+      \code{.cpp}
+        HiveCore* core = HiveCore::Instance();
+        QString method("SomePlugin.SomeMethod");
+        QByteArray param("Some data");
+        QByteArray* result = core->CallPluginMethod(method, &param);
+      \endcode
+      \todo По-возможности заменить тип параметра methodName на <i>const char*</i>
+    */
     QByteArray* CallPluginMethod(const QString& methodName, QByteArray* params);
 
     const QStringList listLoadedMethods() const;
--- src/Core/PluginInterface.h
+++ src/Core/PluginInterface.h
@@ -1,4 +1,12 @@
-﻿#pragma once
+﻿/** \file PluginInterface.h
+  \brief Интерфейс коммуникации ядра с плагином
+  \details Предоставляет интрефейс к плагину:
+  - Экспорт имен методов
+  - Вызов внутренних методов
+  - Макросы для удобной кодогенерации вызовов
+*/
+
+#pragma once
 
 #include <QtPlugin>
 #include <QMap>
@@ -6,12 +14,12 @@
 
 /** Макросы для упрощения вызова экстпортируемых методов */
 //======
-/** @def BEGIN_EXPORTED_SELECTOR_BY(SELECTOR)
+/** \def BEGIN_EXPORTED_SELECTOR_BY(SELECTOR)
   Определяет базовые переменные для выбора нужного вызова
   - result для хранения результата
   - nameDecorator вида "PluginName."
   - macro_MethodName определяет переменную QString с именем вызываемой функции
-  @param SELECTOR определяет переменную QString с именем вызываемой функции
+  \param SELECTOR определяет переменную QString с именем вызываемой функции
  */
 #define BEGIN_EXPORTED_SELECTOR_BY(SELECTOR) \
     QByteArray* result = NULL; \
@@ -19,27 +27,30 @@
     nameDecorator+= "."; \
     QString macro_MethodName = SELECTOR;
 
-/** @def CALL_EXPORTED_FUNC(FNAME)
+/** \def CALL_EXPORTED_FUNC(FNAME)
   Создает код проверяющий соответствие переданного имени функции
     экспортируемым фукнциям и вызывает метод при совпадении, передвавая
     ему параметры
-  @param FNAME имя экспортируемой функции
-  @code{.cpp}
+  \param FNAME имя экспортируемой функции
+  \code{.cpp}
     CALL_EXPORTED_FUNC(SomeFunction)
     // разворачивается в
     if (macro_MethodName  == nameDecorator+ "SomeFunction")
         result = SomeFunction(param);
-  @endcode */
+  \endcode */
 #define CALL_EXPORTED_FUNC(FNAME) \
     if (macro_MethodName  == nameDecorator+ #FNAME) result = FNAME(param);
 
-/** @def CALL_EXPORTED_FUNC_NOPARAMS(FNAME)
+/** \def CALL_EXPORTED_FUNC_NOPARAMS(FNAME)
   Работает так-же как и CALL_EXPORTED_FUNC но прндназначена для методов
-  без параметров
-@ref CALL_EXPORTED_FUNC(FNAME) */
+  без параметров.
+  \ref см CALL_EXPORTED_FUNC(FNAME) */
 #define CALL_EXPORTED_FUNC_NOPARAMS(FNAME) \
     if (macro_MethodName == nameDecorator + #FNAME) result = FNAME();
 
+/** \def RETURN_RESULT
+  выходит из функции, возвращая результат
+*/
 #define RETURN_RESULT \
     return result;
 //======
@@ -47,50 +58,56 @@
 class QStringList;
 class QString;
 
-/** @brief Стандартный интерфейс плагина
-    @detailed Используется QPluginLoader для загрузки плагина и последующего
+/** \brief Стандартный интерфейс плагина
+    \details Используется QPluginLoader для загрузки плагина и последующего
         общения с ним
  */
 class PluginInterface
 {
-    /** @brief Указатель на функцию со стандартным параметром
-        @todo УКАЗАТЕЛЬ НА МЕТОД БЕЗ ПАРАМЕТРОВ! */
+    /** \brief Указатель на функцию со стандартным параметром
+        \todo УКАЗАТЕЛЬ НА МЕТОД БЕЗ ПАРАМЕТРОВ! */
     typedef QByteArray* (*CoreCallbackFunc)(const QString& id, QByteArray* param);
 public:
     virtual ~PluginInterface(){}
 
-    /** @brief Имя плагина. Им же декорируются имена экспортируемых функций
-        @code
+    /** \brief Имя плагина. Им же декорируются имена экспортируемых функций
+        \return Возврашает текстовый идентификатор плагина
+        \code
             "SomePlugin"
-        @endcode */
+        \endcode */
     const virtual QString getPluginId() const = 0;
 
-    /** @brief Возвращает список декорированных имен методов,
+    /** \brief Возвращает список декорированных имен методов,
             экспортируемых плагином
-        @code{.cpp}
+        \code{.cpp}
             ["SomePlugin.SomeMethod1","SomePlugin.SomeMethod2"]
-        @endcode */
+        \endcode */
     virtual QStringList getPluginMethods() = 0;
 
-    /** @brief Через этот метод вызываются экспортируемые методы
-      @param methodName Строка-идентификатор метода. ex "SomePlugin.SomeMethod1"
-      @param param Указатель на передаваемые аргументы. Если вызываемый метод
+    /** \brief Через этот метод вызываются экспортируемые методы
+      \param methodName Строка-идентификатор метода. ex "SomePlugin.SomeMethod1"
+      \param param Указатель на передаваемые аргументы. Если вызываемый метод
         плагина не принимает параметры, то игнорируется.
-      @code{.cpp}
+      \return Результат выполнения метода или NULL если метод не найден
+      \todo Может быть вместо NULL кидать исключение?
+
+      \code{.cpp}
+      // пример использования
       QByteArray param("Some argument");
       QByteArray* result = pluginInstance->
                                 CallInternal("SomePlugin.SomeMethod1", &param);
-      @endcode */
+      \endcode */
     virtual QByteArray* CallInternal(const QString methodName, QByteArray* param = NULL) = 0;
 
-    /** @brief Используется для установки указателя для вызова функции из других
-        модулей или ядра. */
-    void SetCoreCallback(CoreCallbackFunc);
+    /** \brief Используется для установки указателя для вызова функции из других
+        модулей или ядра.
+        \param callback Указатель на функцию обрабатывающую внешние вызовы */
+    void SetCoreCallback(CoreCallbackFunc callback);
 
 protected:
-    /** @brief Декорирует имена экспортируемых методов именем плагина и точкой
-        @param methodNames список недекорированных имен методов
-        @todo перенести в приват и убрать параметр pluginId
+    /** \brief Декорирует имена экспортируемых методов именем плагина и точкой
+        \param methodNames список недекорированных имен методов
+        \todo перенести в приват и убрать параметр pluginId
     */
     void DecorateMethodNames(QStringList& methodNames, QString pluginId) const {
         for(QStringList::iterator methodName = methodNames.begin();
@@ -100,12 +117,12 @@ protected:
         }
     }
 
-    /** @brief Указатель на функцию вызова методов из других плагинов */
+    /** \brief Указатель на функцию вызова методов из других плагинов */
     CoreCallbackFunc CallExternal;
 
-    /** @todo deprecated */
+    /** \todo deprecated */
     QStringList exportingMethods;
 };
 
-/** @def Объявляет Qt что данный интерфейс плагиновый и присваиваем ему id*/
+/** \def Объявляет Qt что данный интерфейс плагиновый и присваиваем ему id*/
 Q_DECLARE_INTERFACE(PluginInterface, "com.monolith.Plugin.HiveInterface/0.4")
