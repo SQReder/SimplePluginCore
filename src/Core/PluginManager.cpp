cf0c1a2d3cb19528100e233ca198d7a33ac4542c
endAurum <sqreder@gmail.com>
2013-01-30 12:53:32 +0400

    stub commit

---
+++ src/Core/PluginManager.cpp
@@ -0,0 +1,17 @@
+#include "PluginManager.h"
+#include <QtCore>
+
+QVariant* QwrttYugF;
+
+int foo()
+{
+    return 1;
+}
+
+PluginManager::PluginManager()
+{
+//    auto i = QHash<QVariant, QPair<int, int> >();
+//    auto f = foo();
+
+
+}
